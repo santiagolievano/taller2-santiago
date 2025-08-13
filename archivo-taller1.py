3b947179e4b88fb402efe0c280a5012845d10ecf
santiagolievano <s.lievanom@uniandes.edu.co>
2025-08-13 09:59:21 -0500

    Creación de dos líneas de código.

--- archivo-taller1.py
+++ archivo-taller1.py
@@ -5,3 +5,7 @@ df = pd.read_csv("BikePrices.csv")
 print(df.isna().sum())
 
 print("El tamaño del DataFrame es:", df.shape)
+
+print("Las primeras 5 filas del DataFrame son:", df.head())
+
+print("Las últimas 5 filas del DataFrame son:", df.tail())
